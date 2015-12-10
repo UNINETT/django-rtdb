76da955278d3281dadaad001a9415d9ed03f5b4b
Hanne Moa <hanne.moa@uninett.no>
2015-12-10 10:59:40 +0100

    Bugfix-version v0.2.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst') as README_FILE:
 
 setup(
     name='django-rtdb',
-    version='0.2',
+    version='0.2.1',
     packages=['rtdb'],
     package_dir = {'': 'src',},
     include_package_data=True,
