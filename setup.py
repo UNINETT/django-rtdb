a8af13c1756581fee0a02a9da9cbb4d252d77dab
Hanne Moa <hanne.moa@uninett.no>
2015-12-15 12:58:04 +0100

    Bump version: 1.0.1 → 1.0.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst') as README_FILE:
 
 setup(
     name='django-rtdb',
-    version='1.0.1',
+    version='1.0.2',
     packages=['rtdb'],
     package_dir = {'': 'src',},
     include_package_data=True,
